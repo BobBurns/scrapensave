8609bb4301b971ee995289a90ab2db7e293e2f01
Bob Burns <bob.burns22@icloud.com>
2017-02-12 14:25:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+#**ScrapeNSave**
+A Web Scraper/Archiver written in Go.
+
+##**Usage**
+```Usage ./scrapensave url
+  -a path
+    	archive path. Default ./Archive
+  -e file
+    	exclude file type from archive (ex. "txt|mov")
+  -f file
+    	file of links to archive
+  -n	do not archive
+  -p milliseconds
+    	polite crawl delay milliseconds (default 500)```
+
+Spider and save links to file
+```./scrapensave -n http://www.example.com > links.txt```
+or Archive from list of links
+```./scrapensave -f links.txt```
+Exclude large files from Spidering or Archiving
+```./scrapensave -e "mov|jpeg|mp3"```
+
+ScrapeNSave respects robots.txt
+
+##**Installation**
+Precompiled binary for Mac 10.11.6 or greater in Binaries folder
---
+++ utils.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
+func fixUrl(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
+}
+
+func savePage(uri string, httpBody io.Reader) error {
+	b, err := ioutil.ReadAll(httpBody)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading body ", err)
+		return err
+	}
+	full, err := url.Parse(uri)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "problem with parsing url")
+		return err
+	}
+
+	savePath := PATH + full.Path
+	err = os.MkdirAll(filepath.Dir(savePath), os.ModePerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating Path %s\n", err)
+		return err
+	}
+	fmt.Printf("Saving file: %s\n", savePath)
+	f, err := os.Create(savePath)
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating file: %s\n", err)
+		return err
+	}
+	n, err := f.Write(b)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error Writing %s:%s\n", savePath, err)
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "Saving %d bytes to %s\n", n, savePath)
+
+	return nil
+}
+
+func fetch(uri string) (io.Reader, error) {
+	fmt.Fprintf(os.Stderr, "#")
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client := http.Client{Transport: transport}
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// more politeness
+	req.Header.Set("User-Agent", AGENT)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
